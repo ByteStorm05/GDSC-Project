aaea1d6ee5536cbf564d56b5aab2fe7f2805f8b3
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2024-10-23 09:32:11 +0530

    auth

--- src/auth.ts
+++ src/auth.ts
@@ -5,10 +5,6 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    })],
+  providers: [Google],
   secret: process.env.NEXTAUTH_SECRET
 });
